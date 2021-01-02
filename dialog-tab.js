882155b822ff303bb3c4bccb2179909e91b7a5e0
Bereketab Gulai <biruktes@outlook.com>
2021-01-02 23:15:38 +0000

    Update dialog tab to add back and forward btns

--- dialog-tab.js
+++ dialog-tab.js
@@ -24,8 +24,7 @@
                     createOrRemoveContextMenuSelectSearch(changes.SEARCH_ENGINE_COLLECTION.oldValue)
                 }
             });
-        }
-        else {
+        } else {
             setTimeout(wait, 300);
         }
     }, 300);
@@ -50,6 +49,16 @@
             'title': '[Dialog Tab] Search with',
             'contexts': ['selection']
         });
+        chrome.contextMenus.create({
+            'id': 'media-dialog-tab',
+            'title': '[Dialog Tab] View',
+            'contexts': ['image', 'video', 'audio']
+        });
+        chrome.contextMenus.create({
+            'id': 'page-dialog-tab',
+            'title': '[Dialog Tab] Review',
+            'contexts': ['page']
+        });
 
         createContextMenuSelectSearch();
 
@@ -64,6 +73,10 @@
                     } else if (itemInfo.parentMenuItemId === 'select-search-dialog-tab') {
                         var engineId = itemInfo.menuItemId.substr(itemInfo.parentMenuItemId.length);
                         dialogTabSearch(engineId, itemInfo.selectionText);
+                    } else if (itemInfo.parentMenuItemId === 'media-dialog-tab') {
+                        dialogTab(itemInfo.srcUrl);
+                    } else if (itemInfo.parentMenuItemId === 'page-dialog-tab') {
+                        dialogTab(itemInfo.pageUrl);
                     }
                 }
             });
@@ -153,6 +166,13 @@
         webview.style.overflow = "hidden";
         webview.style.borderRadius = "10px";
 
+
+        webview.addEventListener('permissionrequest',
+            function (e) {
+                // if (e.permission === 'media') {
+                e.request.allow();
+                // }
+            });
         webview.addEventListener("loadstart", function () {
             this.style.backgroundColor = "white";
             document.getElementById("progressBar-" + webviewId).style.display = "block";
@@ -175,7 +195,7 @@
         divOptionContainer.innerHTML = getEllipsisContent();
 
         divOptionContainer.firstElementChild.addEventListener("mouseover", function () {
-            showWebviewOptions(webviewId, divOptionContainer);
+            showWebviewOptions(webview, divOptionContainer);
         });
         //#endregion
 
@@ -211,10 +231,10 @@
         progressBar.style.borderRadius = "5px";
         //#endregion
 
-        progressBarContainer.appendChild(progressBar);
         divContainer.appendChild(divOptionContainer);
-        divContainer.appendChild(webview);
+        progressBarContainer.appendChild(progressBar);
         divContainer.appendChild(progressBarContainer);
+        divContainer.appendChild(webview);
 
         // Get for current tab and append divContainer
         document.getElementsByClassName('active visible webpageview')[0].appendChild(divContainer);
@@ -222,19 +242,21 @@
 
     /**
      * Displays open in tab buttons and current url in input element
-     * @param {string} webviewId is the id of the webview
+     * @param {string} webview is the webview
      * @param {Object} thisElement the current instance divOptionContainer (div) element
      */
-    function showWebviewOptions(webviewId, thisElement) {
-        var inputId = "input-" + webviewId;
-        console.log((document.getElementById(inputId) === null), webviewId);
+    function showWebviewOptions(webview, thisElement) {
+        var inputId = "input-" + webview.id;
+        console.log((document.getElementById(inputId) === null), webview.id);
         if (document.getElementById(inputId) === null) {
-            var webviewSrc = document.getElementById(webviewId).src;
             var input = document.createElement('input', 'text');
             var buttonNewTab = document.createElement('button');
             var buttonBackgroundTab = document.createElement('button');
+            var breakLine = document.createElement('br');
+            var buttonBack = document.createElement('button');
+            var buttonForward = document.createElement('button');
 
-            input.value = webviewSrc;
+            input.value = webview.src;
             input.id = inputId;
             input.setAttribute("readonly", "");
             input.style.background = "transparent";
@@ -242,6 +264,8 @@
             input.style.border = "unset";
             input.style.width = "20%";
             input.style.margin = "0 0.5rem 0 0.5rem";
+
+
             buttonNewTab.style.background = "transparent";
             buttonNewTab.style.margin = "0 0.5rem 0 0.5rem";
             buttonNewTab.style.border = "unset";
@@ -262,11 +286,30 @@
                 }
             });
 
+            buttonBack.style.background = "transparent";
+            buttonBack.style.margin = "0 0.5rem 0 0.5rem";
+            buttonBack.style.border = "unset";
+            buttonBack.innerHTML = getBackBtnContent();
+            buttonBack.addEventListener("click", function (event) {
+                webview.back();
+            });
+
+            buttonForward.style.background = "transparent";
+            buttonForward.style.margin = "0 0.5rem 0 0.5rem";
+            buttonForward.style.border = "unset";
+            buttonForward.innerHTML = getForwardBtnContent();
+            buttonForward.addEventListener("click", function (event) {
+                webview.forward();
+            });
+
             thisElement.appendChild(buttonNewTab);
             thisElement.appendChild(buttonBackgroundTab);
             thisElement.appendChild(input);
+            thisElement.appendChild(breakLine);
+            thisElement.appendChild(buttonBack);
+            thisElement.appendChild(buttonForward);
 
-            console.log(webviewSrc, thisElement);
+            console.log(webview.src, thisElement);
         }
     }
 
@@ -288,7 +331,7 @@
      * Opens a new chrome tab with specified active boolean value
      * @param {string} inputId is the id of the input containing current url
      * @param {boolean} active indicates whether the tab is active or not (background tab)
-    */
+     */
     function openNewTab(inputId, active) {
         var url = document.getElementById(inputId).value;
 
@@ -317,5 +360,20 @@
      */
     function getBacktabContent() {
         return "<svg aria-hidden=\"true\" focusable=\"false\" data-prefix=\"fas\" data-icon=\"external-link-square-alt\" class=\"svg-inline--fa fa-external-link-square-alt fa-w-14\" style=\"width: 21px;\" role=\"img\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 448 512\"><path fill=\"currentColor\" d=\"M448 80v352c0 26.51-21.49 48-48 48H48c-26.51 0-48-21.49-48-48V80c0-26.51 21.49-48 48-48h352c26.51 0 48 21.49 48 48zm-88 16H248.029c-21.313 0-32.08 25.861-16.971 40.971l31.984 31.987L67.515 364.485c-4.686 4.686-4.686 12.284 0 16.971l31.029 31.029c4.687 4.686 12.285 4.686 16.971 0l195.526-195.526 31.988 31.991C358.058 263.977 384 253.425 384 231.979V120c0-13.255-10.745-24-24-24z\"></path></svg>";
+    }
+
+
+    /** 
+     * Returns string of chevron left svg icon
+     */
+    function getBackBtnContent() {
+        return "<svg aria-hidden=\"true\" focusable=\"false\" data-prefix=\"fas\" data-icon=\"chevron-left\" class=\"svg-inline--fa fa-chevron-left fa-w-10\" style=\"width: 21px;\" role=\"img\" xmlns=\"http:\/\/www.w3.org\/2000\/svg\" viewBox=\"0 0 320 512\"><path fill=\"currentColor\" d=\"M34.52 239.03L228.87 44.69c9.37-9.37 24.57-9.37 33.94 0l22.67 22.67c9.36 9.36 9.37 24.52.04 33.9L131.49 256l154.02 154.75c9.34 9.38 9.32 24.54-.04 33.9l-22.67 22.67c-9.37 9.37-24.57 9.37-33.94 0L34.52 272.97c-9.37-9.37-9.37-24.57 0-33.94z\"></path></svg>";
+    }
+
+    /** 
+     * Returns string of chevron right svg icon
+     */
+    function getForwardBtnContent() {
+        return "<svg aria-hidden=\"true\" focusable=\"false\" data-prefix=\"fas\" data-icon=\"chevron-right\" class=\"svg-inline--fa fa-chevron-right fa-w-10\" style=\"width: 21px;\" role=\"img\" xmlns=\"http:\/\/www.w3.org\/2000\/svg\" viewBox=\"0 0 320 512\"><path fill=\"currentColor\" d=\"M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z\"></path></svg>";
     }
 })();
